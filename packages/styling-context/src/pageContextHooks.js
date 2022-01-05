934da898c2dc1a0419292341a762b2ec2019b5a8
Cenk Cetinkaya <mail@cenkcetinkaya.com>
2022-01-05 13:09:02 +0300

    refactor: Improve styles diff

--- packages/styling-context/src/pageContextHooks.js
+++ packages/styling-context/src/pageContextHooks.js
@@ -8,71 +8,88 @@ export function pageContextHooks(hook) {
 				return buildProps(styles);
 			};
 		case 'reduceDiffStyleHook':
-			return function reduceDiffStyleHook(oldStyles, newStyles) {
-				function isEqual(oldStyle, newStyle) {
+			return function getStylesDiff(
+				oldStyles,
+				newStyles
+			) {
+				function isEqual(
+					oldStyle,
+					newStyle
+				) {
 					if (oldStyle === undefined) {
 						return false;
 					}
-
+			
 					var keys1 = Object.keys(oldStyle);
 					var keys2 = Object.keys(newStyle);
-
+			
 					if (keys1.length !== keys2.length) {
 						return false;
 					}
-
+			
 					let res = keys2.some(function (key) {
 						return oldStyle[key] !== newStyle[key];
 					});
-
+			
 					return !res;
 				}
-
-				return function diffStylingReducer(acc, key) {
-					// align is readolnly issue on Android
-					if (key === 'align') {
-						acc[key] = undefined;
-						return acc;
-					}
-					else if (key === "layout") {
-						var diffReducer = reduceDiffStyleHook(oldStyles[key] || {}, newStyles[key] || {});
-						Object.keys(newStyles[key] || {}).reduce(diffReducer, acc[key] = {});
-					}
-					else if (key == "flexProps" && newStyles[key]) {
-						Object.keys(newStyles[key])
-							.forEach(function (name) {
-								if (oldStyles[key] === undefined || newStyles[key][name] !== oldStyles[key][name]) {
-									acc[name] = newStyles[key][name];
-
-									if (newStyles[key][name] === null) {
-										acc[name] = NaN;
-										// fixes flexgrow NaN value bug
-										if (name === "flexGrow") {
-											acc[name] = 0;
+			
+				return function diffStylingReducer() {
+					return Object.keys(newStyles).reduce(
+						(acc, key) => {
+							// align is readolnly issue on Android
+							if (key === "align") {
+								acc[key] = undefined;
+								return acc;
+							} else if (key === "layout") {
+								var diffReducer = getStylesDiff(
+									oldStyles[key] || {},
+									newStyles[key] || {}
+								);
+								diffReducer();
+							} else if (key == "flexProps" && newStyles[key]) {
+								Object.keys(newStyles[key]).forEach(function (name) {
+									if (
+										oldStyles[key] === undefined ||
+										newStyles[key][name] !== oldStyles[key][name]
+									) {
+										acc[name] = newStyles[key][name];
+			
+										if (newStyles[key][name] === null) {
+											acc[name] = NaN;
+											// fixes flexgrow NaN value bug
+											if (name === "flexGrow") {
+												acc[name] = 0;
+											}
+										} else {
+											acc[name] = newStyles[key][name];
 										}
 									}
-									else {
-										acc[name] = newStyles[key][name];
-									}
+								});
+							} else if (
+								newStyles[key] !== null &&
+								newStyles[key] instanceof Object
+							) {
+								if (
+									Object.keys(newStyles[key]).length > 0 &&
+									!isEqual(oldStyles[key] || {}, newStyles[key])
+								) {
+									acc[key] = merge(oldStyles[key], newStyles[key]);
 								}
-							});
-					}
-					else if (newStyles[key] !== null && newStyles[key] instanceof Object) {
-						if (Object.keys(newStyles[key]).length > 0 && !isEqual(oldStyles[key] || {}, newStyles[key])) {
-							acc[key] = merge(oldStyles[key], newStyles[key]);
-						}
-					}
-					else if (oldStyles[key] !== newStyles[key]) {
-						acc[key] = newStyles[key];
-					}
-
-					if (acc[key] === null) {
-						acc[key] = NaN;
-					}
-
-					return acc;
+							} else if (oldStyles[key] !== newStyles[key]) {
+								acc[key] = newStyles[key];
+							}
+			
+							if (acc[key] === null) {
+								acc[key] = NaN;
+							}
+			
+							return acc;
+						},
+						{}
+					);
 				};
-			};
+			}
 	}
 
 	return undefined;
