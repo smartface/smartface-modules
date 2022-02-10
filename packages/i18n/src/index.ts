4cbdbd1a34d0c81a27cfabf5682b452bd3ada961
alimoncul <alimoncul@gmail.com>
2022-02-10 10:09:07 +0300

    feature: change i18n

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,73 +1,66 @@
-import '@formatjs/intl-getcanonicallocales/polyfill';
-import '@formatjs/intl-locale/polyfill';
-import '@formatjs/intl-pluralrules/polyfill';
-import '@formatjs/intl-relativetimeformat/polyfill';
-import '@formatjs/intl-listformat/polyfill';
-import '@formatjs/intl-displaynames/polyfill';
-import '@formatjs/intl-numberformat/polyfill';
-import '@formatjs/intl-numberformat/locale-data/tr';
-import '@formatjs/intl-datetimeformat/polyfill';
+import { EventEmitter } from "@smartface/native/core/eventemitter";
 
-import { createIntl, createIntlCache, IntlShape } from '@formatjs/intl';
-import { EventEmitter } from '@smartface/native/core/eventemitter';
+const PROPERTY_CAPTURE_REGEXP = /[^{\}]+(?=})/g;
 
 export class i18n {
-	private intlCache = createIntlCache();
-	private emitter = new EventEmitter();
-	private languageFiles: {
-		locale: string;
-		messages: Record<string, string>;
-	}[];
-	private defaultLocale = 'en';
-	protected _language: IntlShape<string>;
-	protected _onError: (...args: any[]) => void;
-	static instance: i18n;
-	constructor(props: {
-		languageFiles: { locale: string; messages: Record<string, string> }[];
-		defaultLocale: string;
-	}) {
-		if (i18n.instance) {
-			throw new Error('Cannot be instantiated more');
-		}
-		this.languageFiles = props.languageFiles || [];
-		this.defaultLocale = props?.defaultLocale || this.defaultLocale;
-		this.locale = this.defaultLocale;
-		i18n.instance = this;
-	}
-	onChange(callback: (...args: any[]) => void) {
-		return this.emitter.on('change', callback);
-	}
-	set onError(value: (...args: any[]) => void) {
-		this._onError = value;
-	}
-	get language(): IntlShape<string> {
-		return this._language;
-	}
-	get locale(): string {
-		return this._language.locale;
-	}
-	set locale(locale: string) {
-		const messages = this.languageFiles.find(
-			(lf) => lf.locale === locale
-		)?.messages;
-		if (messages) {
-			this._language = createIntl(
-				{
-					defaultLocale: 'en',
-					locale: locale,
-					messages: messages,
-					onError:
-						this._onError ||
-						function (err) {
-							console.error(err);
-						},
-					fallbackOnEmptyString: true,
-				},
-				this.intlCache
-			);
-			this.emitter.emit('change');
-		} else {
-			throw new Error('Locale not found: ' + locale);
-		}
-	}
+  private emitter = new EventEmitter();
+  private languageFiles: {
+    locale: string;
+    messages: Record<string, string>;
+  }[];
+  private defaultLocale = "en";
+  private currentLocale = "en";
+  protected _language: Record<string, string>;
+  static instance: i18n;
+  constructor(props: { languageFiles: { locale: string; messages: Record<string, string> }[]; defaultLocale: string }) {
+    if (i18n.instance) {
+      throw new Error("Cannot be instantiated more.");
+    }
+    this.languageFiles = props?.languageFiles || [];
+    this.defaultLocale = props?.defaultLocale || this.defaultLocale;
+    this.locale = this.defaultLocale;
+    i18n.instance = this;
+  }
+  protected propsChecker(value: string, props: Record<string, string>) {
+    const propsNeeded = value.match(PROPERTY_CAPTURE_REGEXP) || [];
+    const propsGot = Object.keys(props);
+    const propsGood = propsNeeded.every(p => propsGot.includes(p));
+    return {
+      propsNeeded,
+      propsGot,
+      propsGood
+    };
+  }
+  onChange(callback: (...args: any[]) => void) {
+    return this.emitter.on("change", callback);
+  }
+  getMessage(key: string, props?: Record<string, string>): string {
+    const value = this._language[key];
+    if (value) {
+      if ((value.indexOf("{") > -1 || value.indexOf("}") > -1) && props) {
+        const { propsNeeded, propsGot, propsGood } = this.propsChecker(value, props);
+        if (propsGood) {
+          return value.replace(PROPERTY_CAPTURE_REGEXP, p => props[p]);
+        } else {
+          throw new Error(`Correct props not given for key: ${key} | PropsNeeded: ${propsNeeded} | PropsGot: ${propsGot}.`);
+        }
+      }
+      return value;
+    } else {
+      throw new Error(`No value found with key: ${key}.`);
+    }
+  }
+  get locale(): string {
+    return this.currentLocale;
+  }
+  set locale(locale: string) {
+    const messages = this.languageFiles.find(lf => lf.locale === locale)?.messages;
+    if (messages) {
+      this._language = messages;
+      this.currentLocale = locale;
+    } else {
+      throw new Error(`Locale not found: ' ${locale}.`);
+    }
+    this.emitter.emit("change");
+  }
 }
